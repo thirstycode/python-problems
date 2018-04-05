f8edc9496f7e9db8e1ffb7ce6fd903d0ba62cf9a
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-05 17:05:03 +0530

    Update sol.py

--- sum of square and cube/sol.py
+++ sum of square and cube/sol.py
@@ -1,5 +1,7 @@
 x = input("Enter A Number : ")
 x = int(x)
-for i in range(int(int(x)**(0.25)),int(int(x)**(1/3)) + 1):
-    if  i*i + i*i*i == x :
-        print (str(i) + "**2 + " + str(i) + "**3 = " + str (x))
+for i in range(1,int(x)):
+    p = i*i*i
+    for j in range(1,int(x-p)):
+        if j*j == x-p:
+            print(str(j) + "**2 + " + str(i) + "**3 = " + str(x) )
