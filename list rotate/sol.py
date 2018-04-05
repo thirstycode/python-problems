ca5186751b24bc68bc15ec652980702063983823
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-05 17:20:04 +0530

    Update sol.py

--- list rotate/sol.py
+++ list rotate/sol.py
@@ -1,9 +1,12 @@
-list1 = [int(x) for x in input().split()]
-rotate1 = input("")
+list1 = [int(x) for x in input("Enter List Elements Seperated By Space : ").split()]
+rotate1 = input("Rotate By : ")
 rotate1 = int(rotate1)
 if rotate1 > 0 :
-    rotate1 = (rotate1)%len(list1) - 1
-    list1 = list1[rotate1:] + list1[:len(list1)-rotate1-1]
+    rotate1 = rotate1%len(list1)
+    for i in range (0,rotate1):
+        t = list1[-1]
+        list1.pop()
+        list1.insert(0,t)
     print(list1)
 else :
     print(list1)
